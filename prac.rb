b6c982551195b0052413b5532c5a781640911562
Alyssa Yates <bosslys@icloud.com>
2019-09-14 19:32:02 +0000

    Automatically backed up by Learn

--- prac.rb
+++ prac.rb
@@ -1,13 +1,11 @@
 groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
 
-groceries.values 
-groceries.keys 
+groceries.values
+groceries.keys
 
 letters = {a: 1, b: 2}
 
-letters.min 
+letters.min
 
 alphabetical_order = {c: 100, d: 1}
-alphabetical_order.min 
-
-
+alphabetical_order.min
